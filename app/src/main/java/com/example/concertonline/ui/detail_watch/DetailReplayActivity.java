b4f3997553b069d9b7b75b1485e1b8c41b0efc00
frisdiandi <frisdiandiseptiatama@gmail.com>
2020-10-12 07:24:57 +0700

    Watch Fix

--- app/src/main/java/com/example/concertonline/ui/detail_watch/DetailReplayActivity.java
+++ app/src/main/java/com/example/concertonline/ui/detail_watch/DetailReplayActivity.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -19,6 +20,7 @@ public class DetailReplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_replay);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         btnClose = (TextView)findViewById(R.id.btnClose);
         btnClose.setOnClickListener(new View.OnClickListener() {
